7e06ae851f9f0cf7ebfee6de123720a5b67f6072
Gabriel Barbosa <gabrielbbl@live.com>
2020-08-22 23:03:27 -0300

    shop page mobile ready

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,26 @@
 		});
 
 		/* SHOP PAGE - PRODUCTS LOOP */
+		/* When the user clicks on the button,
+		toggle between hiding and showing the dropdown content */
+		$('.dropbtn').click(function() {
+		  document.getElementById("myDropdown").classList.toggle("show");
+		});
+
+		// Close the dropdown menu if the user clicks outside of it
+		window.onclick = function(event) {
+		  if (!event.target.matches('.dropbtn')) {
+		    var dropdowns = document.getElementsByClassName("dropdown-content");
+		    var i;
+		    for (i = 0; i < dropdowns.length; i++) {
+		      var openDropdown = dropdowns[i];
+		      if (openDropdown.classList.contains('show')) {
+		        openDropdown.classList.remove('show');
+		      }
+		    }
+		  }
+		}
+
 		$(window).scroll(function(){
 		  $('.product-item').each(function(){
 		    if(isScrolledIntoView($(this))){
--- woocommerce/loop/orderby.php
+++ woocommerce/loop/orderby.php
@@ -37,7 +37,35 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<div class="cat-item <?php echo($term->slug); if($currentcat == $term->slug): echo(' current'); endif; ?>">
 		<a href="<?php $term_link = get_term_link( $term ); echo($term_link); ?>"><?php echo($term->name); ?></a>
 	</div>
-	<?php endforeach; wp_reset_query(); ?>
+	<?php endforeach; ?>
+
+	<div class="cat-item orderby-form">
+		<form class="woocommerce-ordering" method="get">
+			<select name="orderby" class="orderby" aria-label="<?php esc_attr_e( 'Shop order', 'woocommerce' ); ?>">
+				<?php foreach ( $catalog_orderby_options as $id => $name ) : ?>
+					<option value="<?php echo esc_attr( $id ); ?>" <?php selected( $orderby, $id ); ?>><?php echo esc_html( $name ); ?></option>
+				<?php endforeach; ?>
+			</select>
+			<input type="hidden" name="paged" value="1" />
+			<?php wc_query_string_form_fields( null, array( 'orderby', 'submit', 'paged', 'product-page' ) ); ?>
+		</form>
+	</div>
+</div>
+
+<div class="product-cat-menu mobile">
+	<div class="cat-item dropdown">
+	  <button onclick="myFunction()" class="dropbtn">Category: <?php if(is_shop()): ?>All<?php else: echo $cat_obj->name; endif; ?></button>
+	  <div id="myDropdown" class="dropdown-content">
+			<div class="cat-item all">
+				<a href="<?php bloginfo('url'); ?>/editions/">All</a>
+			</div>
+			<?php foreach( $terms as $term ): ?>
+			<div class="cat-item <?php echo($term->slug); if($currentcat == $term->slug): echo(' current'); endif; ?>">
+				<a href="<?php $term_link = get_term_link( $term ); echo($term_link); ?>"><?php echo($term->name); ?></a>
+			</div>
+			<?php endforeach; ?>
+	  </div>
+	</div>
 
 	<div class="cat-item orderby-form">
 		<form class="woocommerce-ordering" method="get">
