4ce10fd699ead8e160eca52200a5b9fe59409c08
Gabriel Barbosa <gabrielbbl@live.com>
2020-08-31 12:30:00 -0300

    cart mobile

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -44,7 +44,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 				if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
 					$product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key );
 					?>
-					<tr class="woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
+					<tr class="woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?> desktop">
 
 						<td class="product-remove">
 							<?php
@@ -128,6 +128,93 @@ do_action( 'woocommerce_before_cart' ); ?>
 							?>
 						</td>
 					</tr>
+
+					<?php if (wp_is_mobile()): ?>
+					<!--- MOBILE -->
+					<tr class="woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?> mobile">
+
+						<td class="product-remove">
+							<span><?php esc_html_e( 'Product', 'woocommerce' ); ?></span>
+							<?php
+								echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+									'woocommerce_cart_item_remove_link',
+									sprintf(
+										'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
+										esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
+										esc_html__( 'Remove this item', 'woocommerce' ),
+										esc_attr( $product_id ),
+										esc_attr( $_product->get_sku() )
+									),
+									$cart_item_key
+								);
+							?>
+						</td>
+
+						<td class="product-name">
+						<?php
+						if ( ! $product_permalink ) {
+							echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) . '&nbsp;' );
+						} else {
+							echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', sprintf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $_product->get_name() ), $cart_item, $cart_item_key ) );
+						}
+
+						do_action( 'woocommerce_after_cart_item_name', $cart_item, $cart_item_key );
+
+						// Meta data.
+						echo wc_get_formatted_cart_item_data( $cart_item ); // PHPCS: XSS ok.
+
+						// Backorder notification.
+						if ( $_product->backorders_require_notification() && $_product->is_on_backorder( $cart_item['quantity'] ) ) {
+							echo wp_kses_post( apply_filters( 'woocommerce_cart_item_backorder_notification', '<p class="backorder_notification">' . esc_html__( 'Available on backorder', 'woocommerce' ) . '</p>', $product_id ) );
+						}
+						?>
+						</td>
+
+						<td class="product-price-qty">
+							<div class="product-price">
+								<div class="label"><?php esc_html_e( 'Price', 'woocommerce' ); ?></div>
+								<?php
+									echo apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
+								?>
+							</div>
+
+							<div class="product-quantity">
+								<div class="label"><?php esc_html_e( 'Quantity', 'woocommerce' ); ?></div>
+								<?php
+								if ( $_product->is_sold_individually() ) {
+									$product_quantity = sprintf( '1 <input type="hidden" name="cart[%s][qty]" value="1" />', $cart_item_key );
+								} else {
+									$product_quantity = woocommerce_quantity_input(
+										array(
+											'input_name'   => "cart[{$cart_item_key}][qty]",
+											'input_value'  => $cart_item['quantity'],
+											'max_value'    => $_product->get_max_purchase_quantity(),
+											'min_value'    => '0',
+											'product_name' => $_product->get_name(),
+										),
+										$_product,
+										false
+									);
+								}
+
+								echo apply_filters( 'woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item ); // PHPCS: XSS ok.
+								?>
+							</div>
+						</td>
+
+						<td class="product-thumbnail" style="background: linear-gradient(180deg, <?php the_field('background_color', $cart_item['product_id']); ?> 10%, #fff 80%) !important; background: -moz-linear-gradient(180deg, <?php the_field('background_color', $cart_item['product_id']); ?> 10%, #fff 80%); background: -webkit-linear-gradient(270deg, <?php the_field('background_color', $cart_item['product_id']); ?> 10%, #fff 80%); background: -o-linear-gradient(180deg, <?php the_field('background_color', $cart_item['product_id']); ?> 10%, #fff 80%);">
+						<?php
+						$thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
+
+						if ( ! $product_permalink ) {
+							echo $thumbnail; // PHPCS: XSS ok.
+						} else {
+							printf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $thumbnail ); // PHPCS: XSS ok.
+						}
+						?>
+						</td>
+					</tr>
+					<?php endif; ?>
 					<?php
 				}
 			}
